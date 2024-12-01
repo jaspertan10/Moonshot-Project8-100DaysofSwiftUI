99b3da2997a1c655feee978e8e2fd318cde1b955
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-01 15:37:13 -0800

    Added mission and astronaut structs with JSON decoding extension for Bundle. Implement base ContentView UI

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jasper Tan on 12/1/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Mission.swift
@@ -0,0 +1,33 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Jasper Tan on 12/1/24.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+    
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+}
