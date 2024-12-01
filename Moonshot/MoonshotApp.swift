cff4875491e5c001c88c9428a11c7563e0486292
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-30 16:35:13 -0800

    initial commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jasper Tan on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
